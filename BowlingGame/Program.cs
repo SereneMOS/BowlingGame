6ec44c8bbda270e5bee0960ec26684ad43bd2505
rose.flowers <rosalineaflow@gmail.com>
2021-09-13 13:50:58 -0400

    New content

--- BowlingGame/Program.cs
+++ BowlingGame/Program.cs
@@ -21,26 +21,16 @@ namespace BowlingGame
                 int[] roll_2 = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
                 int[] frame_score = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
                 int roll_3 = 0;
-                for (int i = 0; i < 10; i++)
+                int i;
+                for (i = 0; i < 10; i++)
                 {
                     Random rd = new Random();
                     if (are_we_rand == "I")
                     {
-                        string get_roll = "";
-                        while (roll_1[i] <= -1 || roll_1[i] >= 11)
-                        {
-                            get_roll = Console.ReadLine();
-                            roll_1[i] = Int16.Parse(get_roll);
-                        }
-                        if (roll_1[i] != 10)
-                        {
-                            while (roll_2[i] <= -1 || roll_2[i] + roll_1[i] >= 11)
-                            {
-                                get_roll = Console.ReadLine();
-                                roll_2[i] = Int16.Parse(get_roll);
-                            }
-                        }
+                        roll_ball_1(roll_1, i);
+                        roll_ball_2(roll_1, roll_2, i);
                     }
+
                     if (are_we_rand == "R")
                     {
                         roll_1[i] = rd.Next(0, 11);
@@ -51,7 +41,7 @@ namespace BowlingGame
                     {
                         roll_2[i] = 0;
                     }
-                    if (i != 0 && roll_1[i - 1] + roll_2[i - 1] == 10 && roll_1[i - 0] != 10) //spare logic
+                    if (i != 0 && roll_1[i - 1] + roll_2[i - 1] == 10 && roll_1[i - 1] != 10) //spare logic
                     {
                         frame_score[i - 1] += roll_1[i];
                     }
@@ -80,12 +70,8 @@ namespace BowlingGame
                     {
                         if (are_we_rand == "I")
                         {
+                            roll_ball_2(roll_1, roll_2, i);
                             string get_roll = "";
-                            while (roll_2[i] <= -1 || roll_2[i] + roll_1[i] >= 11)
-                            {
-                                get_roll = Console.ReadLine();
-                                roll_2[i] = Int16.Parse(get_roll);
-                            }
                             while (roll_3 <= -1 || roll_3 >= 11)
                             {
                                 get_roll = Console.ReadLine();
@@ -104,32 +90,69 @@ namespace BowlingGame
                 Console.Clear();
                 Console.WriteLine("    1st   2nd   3rd   Frame");
                 Console.WriteLine("-----------------------------");
-                for (int j = 0; j < 10; j++)
-                {
-                    if (j != 9)
-                    {
-                        Console.Write($"{j + 1}) ");
-                        Console.WriteLine($"{roll_1[j]}     {roll_2[j]}            {frame_score[j]}");
-                    }
-                    else
-                    {
-                        Console.Write($"{j + 1}) ");
-                        Console.WriteLine($"{roll_1[j]}     {roll_2[j]}     {roll_3}      {frame_score[j]}");
-                    }
-                }
+                display_score(i, roll_1, roll_2, frame_score, roll_3);
                 Console.WriteLine(total_score);
-                string play_again = "";
-                while (play_again != "Y" && play_again != "N")
+                again(playing);
+            }
+        }
+
+        static int again (bool play)
+        {
+            string play_again = "";
+            while (play_again != "Y" && play_again != "N")
+            {
+                Console.WriteLine("Would you like to play again? (Y/N)");
+                play_again = Console.ReadLine();
+                Console.Clear();
+            }
+            if (play_again == "N")
+            {
+                play = false;
+            }
+            return 0;
+        }
+
+        static int roll_ball_1 (int[] roll1, int j)
+        {
+                string get_roll = "";
+                while (roll1[j] <= -1 || roll1[j] >= 11)
                 {
-                    Console.WriteLine("Would you like to play again? (Y/N)");
-                    play_again = Console.ReadLine();
-                    Console.Clear();
+                    get_roll = Console.ReadLine();
+                    roll1[j] = Int16.Parse(get_roll);
                 }
-                if (play_again == "N")
+            return 0;
+        }
+
+        static int roll_ball_2 (int[] roll1, int[] roll2, int j)
+        {
+            if (roll1[j] != 10)
+            {
+                string get_roll = "";
+                while (roll2[j] <= -1 || roll2[j] + roll1[j] >= 11)
                 {
-                    playing = false;
+                    get_roll = Console.ReadLine();
+                    roll2[j] = Int16.Parse(get_roll);
                 }
             }
+            return 0;
+        }
+
+        static int display_score (int j, int[] a, int[] b, int[] c, int d)
+        {
+            for ( j = 0; j < 10; j++)
+            {
+                if (j != 9)
+                {
+                    Console.Write($"{j + 1}) ");
+                    Console.WriteLine($"{a[j]}     {b[j]}            {c[j]}");
+                }
+                else
+                {
+                    Console.Write($"{j + 1}) ");
+                    Console.WriteLine($"{a[j]}     {b[j]}     {d}      {c[j]}");
+                }
+            }
+            return 0;
         }
     }
 }
